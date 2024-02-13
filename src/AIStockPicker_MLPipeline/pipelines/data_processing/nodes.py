6a5fa764e3c0098d8549ef1e9e7f222e2ae6a919
pdrleal <pdrlealpereira1@gmail.com>
2024-02-13 21:18:34 +0000

    SMA & RSI feature engineering and selection.

--- src/AIStockPicker_MLPipeline/pipelines/data_processing/nodes.py
+++ src/AIStockPicker_MLPipeline/pipelines/data_processing/nodes.py
@@ -1,5 +1,6 @@
 import logging
 
+import numpy as np
 import pandas as pd
 import pandas_ta as ta
 
@@ -33,7 +34,14 @@ def filter_stocks_table_by_index(stocks_df: pd.DataFrame, stock_index) -> pd.Dat
         logger.info("No data found for %s.", stock_index)
         return pd.DataFrame()
 
-    return stocks_df;
+    return stocks_df
+
+
+def treat_missing_values(stock_df: pd.DataFrame) -> pd.DataFrame:
+    logger.info("Replacing sentiments missing values by 0..")
+    stock_df.loc[:, 'news_sentiment'] = stock_df['news_sentiment'].fillna(0)
+
+    return stock_df
 
 
 def compute_percentage_returns(stock_df: pd.DataFrame) -> pd.DataFrame:
@@ -43,48 +51,113 @@ def compute_percentage_returns(stock_df: pd.DataFrame) -> pd.DataFrame:
     return stock_df
 
 
+# https://www.elearnmarkets.com/school/units/swing-trading/moving-average-strategy
+# https://www.investopedia.com/terms/g/goldencross.asp
 def compute_simple_moving_averages(stock_df: pd.DataFrame) -> pd.DataFrame:
-    logger.info("Computing simple moving averages...")
-    # https://www.elearnmarkets.com/school/units/swing-trading/moving-average-strategy
-    stock_df['sma21'] = ta.sma(stock_df['close'], 21)
-    stock_df['sma50'] = ta.sma(stock_df['close'], 50)
-    stock_df['sma100'] = ta.sma(stock_df['close'], 100)
-    # Golden Cross-over can be achieved when SMA21 crosses SMA50 from below
-    stock_df['bullish_sma'] = (stock_df['sma21'] > stock_df['sma50']).astype(int)
-    # Death Cross-over can be achieved when SMA21 crosses SMA50 from above
-    stock_df['bearish_sma'] = (stock_df['sma21'] < stock_df['sma50']).astype(int)
+    logger.info("Calculating optimal moving averages...")
 
-    return stock_df[['sma21', 'sma50', 'sma100', 'bullish_sma']]
+    # Testing with short moving averages of 1,2 and 3 months and long moving averages of 4,5 and 6 months
+    short_sma_days = [22, 44, 66]
+    long_sma_days = [88, 110, 132]
+    bands = np.arange(0.001, 0.01, 0.001)
+
+    best_sma_returns_information_ratio = -1000
+    best_sma = (0, 0, 0)
+    best_sma_signals = []
+    for short_sma in short_sma_days:
+        for long_sma in long_sma_days:
+            for band in bands:
+
+                sma_signals = (ta.sma(stock_df['close'], short_sma) > (1 + band) * ta.sma(stock_df['close'],
+                                                                                          long_sma)).astype(int)
+                sma_returns = stock_df['return'] * sma_signals
+                sma_returns_information_ratio = sma_returns.mean() / sma_returns.std()
+                if sma_returns_information_ratio > best_sma_returns_information_ratio:  # maximization
+                    best_sma_returns_information_ratio = sma_returns_information_ratio
+                    best_sma = (short_sma, long_sma, band)
+                    best_sma_signals = sma_signals
+                logger.info(
+                    "Testing SMA strategy with short_sma=%d, long_sma=%d and band=%.3f. Information Ratio: %.2f",
+                    short_sma, long_sma,
+                    band, sma_returns_information_ratio)
+
+    logger.info("Best SMA strategy found: short_sma=%d, long_sma=%d and band=%.3f. Information Ratio: %.2f",
+                best_sma[0], best_sma[1],
+                best_sma[2], best_sma_returns_information_ratio)
+    stock_df['sma_signals'] = best_sma_signals
+    return stock_df[['sma_signals']]
 
 
 def compute_relative_strength_indexes(stock_df: pd.DataFrame) -> pd.DataFrame:
-    logger.info("Computing relative strength index...")
+    logger.info("Calculating optimal relative strength indexes...")
     # https://admiralmarkets.com/education/articles/forex-indicators/relative-strength-index-how-to-trade-with-an-rsi-indicator
-    stock_df['rsi'] = ta.rsi(stock_df['close'], 14)
-    stock_df['rsi_overbought'] = (stock_df['rsi'] > 70).astype(int)
-    stock_df['rsi_oversold'] = (stock_df['rsi'] < 30).astype(int)
+    # https://medium.com/mudrex/rsi-masterclass-part-3-9f9a5dfe3fca
 
-    return stock_df[['rsi', 'rsi_overbought', 'rsi_oversold']]
+    # Testing with RSIs of 14, 28 and 42 days for lower levels of 20,25,30,35,40 and upper leves of 80,75,70,65,60
+    rsi_short_lengths = [5, 8, 11, 14, 17]
+    rsi_long_lengths = [20, 25, 30, 35, 40, 45]
+    lower_levels = [20, 25, 30, 35, 40, 45]
+    upper_levels = [80, 75, 70, 65, 60, 55]
+
+    best_rsi_returns_information_ratio = -1000
+    best_rsi = (0, 0, 0, 0)
+    best_rsi_signals = []
+
+    for rsi_short_length in rsi_short_lengths:
+        for rsi_long_length in rsi_long_lengths:
+            for lower_level, upper_level in zip(lower_levels, upper_levels):
+
+                rsi_oversold = (ta.rsi(stock_df['close'], length=rsi_short_length) < lower_level)
+                rsi_overbought = (ta.rsi(stock_df['close'], length=rsi_short_length) > upper_level)
+
+                rsi_positive_trend = (ta.rsi(stock_df['close'], length=rsi_long_length) > 50)
+                rsi_negative_trend = (ta.rsi(stock_df['close'], length=rsi_long_length) < 50)
+
+                # rsi_signals_buy = (rsi_oversold & rsi_positive_trend)
+                rsi_signals_buy = rsi_oversold
+                # rsi_signals_sell = (rsi_overbought & rsi_negative_trend)
+                rsi_signals_sell = rsi_overbought
+
+                rsi_signals = pd.Series(index=stock_df.index)
+                rsi_signals.loc[rsi_signals_buy] = 1
+                rsi_signals.loc[rsi_signals_sell] = 0
+
+                # Forward fill the signal column
+                rsi_signals = rsi_signals.ffill().bfill().fillna(0)
+
+                rsi_returns = stock_df['return'] * rsi_signals
+                if rsi_returns.std() != 0:
+                    rsi_returns_information_ratio = rsi_returns.mean() / rsi_returns.std()
+                else:
+                    rsi_returns_information_ratio = 0
+                if rsi_returns_information_ratio > best_rsi_returns_information_ratio:
+                    best_rsi_returns_information_ratio = rsi_returns_information_ratio
+                    best_rsi = (rsi_short_length, rsi_long_length, lower_level, upper_level)
+                    best_rsi_signals = rsi_signals
+                logger.info(
+                    "Testing RSI strategy with rsi_short_length=%d, rsi_long_length=%d, lower_level=%d and "
+                    "upper_level=%d. Information ratio: %.2f",
+                    rsi_short_length, rsi_long_length, lower_level, upper_level, rsi_returns_information_ratio)
+
+    logger.info(
+        "Best RSI strategy found: rsi_short_length=%d, rsi_long_length=%d, lower_level=%d and upper_level=%d. Information ratio: %.2f",
+        best_rsi[0], best_rsi[1], best_rsi[2], best_rsi[3], best_rsi_returns_information_ratio)
+    stock_df['rsi_signals'] = best_rsi_signals
+
+    return stock_df[['rsi_signals']]
 
 
 def merge_dataframes(*df_list) -> pd.DataFrame:
     logger.info("Merging dataframes...")
     stock_df = pd.concat(df_list, axis=1)
-    return stock_df
-
-
-def treat_missing_values(stock_df: pd.DataFrame) -> pd.DataFrame:
     logger.info("Limiting timeframe from the first news forward...")
     stock_df = stock_df.loc[stock_df['datetime'] >= first_date_with_sentiment]
-    logger.info("Propagating sentiment values forward...")
-    stock_df = stock_df.ffill().bfill()
-
     return stock_df
 
 
 def perform_feature_selection(stock_df) -> pd.DataFrame:
     logger.info("Selecting automatically best features...")
-    return stock_df[['return', 'news_sentiment', 'bullish_sma']]
+    return stock_df
 
 
 def scale_data(stock_df) -> pd.DataFrame:
--- src/AIStockPicker_MLPipeline/pipelines/data_processing/pipeline.py
+++ src/AIStockPicker_MLPipeline/pipelines/data_processing/pipeline.py
@@ -13,38 +13,39 @@ def create_pipeline(**kwargs) -> Pipeline:
                 name="filter_stocks_table_by_index",
             ),
             node(
-                func=compute_percentage_returns,
+                func=treat_missing_values,
                 inputs=["stock_table"],
+                outputs="clean_stock_table",
+                name="treat_sentiment_missing_values",
+            ),
+            node(
+                func=compute_percentage_returns,
+                inputs=["clean_stock_table"],
                 outputs="percentage_returns",
                 name="compute_percentage_return",
             ),
             node(
                 func=compute_simple_moving_averages,
-                inputs=["stock_table"],
+                inputs=["percentage_returns"],
                 outputs="simple_moving_averages",
                 name="compute_simple_moving_average",
             ),
             node(
                 func=compute_relative_strength_indexes,
-                inputs=["stock_table"],
+                inputs=["percentage_returns"],
                 outputs="relative_strength_indexes",
                 name="compute_relative_strength_index",
             ),
             node(
                 func=merge_dataframes,
-                inputs=["percentage_returns", "simple_moving_averages", "relative_strength_indexes"],
+                inputs=["percentage_returns","simple_moving_averages", "relative_strength_indexes"],
                 outputs="stock_table_feature_engineering",
-                name="merge_new_features",
+                name="merge_features",
             ),
-            node(
-                func=treat_missing_values,
-                inputs=["stock_table_feature_engineering"],
-                outputs="clean_stock_table",
-                name="treat_missing_values",
-            ),
+
             node(
                 func=perform_feature_selection,
-                inputs=["clean_stock_table"],
+                inputs=["stock_table_feature_engineering"],
                 outputs="stock_table_feature_selection",
                 name="perform_feature_selection"
             ),
